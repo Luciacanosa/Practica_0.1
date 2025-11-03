20fd884c785072b765e2e59a8f7a81e81037419f
lulutrarala <4350lucia@gmail.com>
2025-11-03 14:09:00 +0100

    pagina informacion retoques

--- js/script.js
+++ js/script.js
@@ -104,6 +104,39 @@ window.addEventListener("click", function (event) {
 });
 
 
+// modal info 2
+// Función propia para abrir ventana modal
+function openModalWindow() {
+  let modalWindow = document.querySelector("#modalWindow2");
+  modalWindow.classList.add("show-modal");
+}
+
+let btnCloseModal2 = document.querySelector(
+  "#modalWindow2 > .modal-content > .close"
+);
+let btnCloseModalAccept2 = document.querySelector(
+  "#modalWindow2 > .modal-content > #closeModalAccept"
+);
+
+btnCloseModal2.addEventListener("click", closeModalWindow);
+btnCloseModalAccept2.addEventListener("click", closeModalWindow);
+
+// Función para cerrar la ventana modal
+function closeModalWindow() {
+  let modalWindow = document.querySelector("#modalWindow");
+  modalWindow.classList.remove("show-modal");
+}
+
+// Cerrar ventana modal cuando se detecta click fuera
+window.addEventListener("click", function (event) {
+  // llama solo a la función de cerrar modal siempre que el click no sea en la propia ventana modal
+  let modal = document.querySelector("#modalWindow");
+
+  if (event.target == modal) {
+    closeModalWindow();
+  }
+});
+
 
 // script para la cuenta atrás del festival
 // página principal
